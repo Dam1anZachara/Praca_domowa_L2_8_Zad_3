23640100cd86b71677a572ae51aab71a47cb1955
Damian Z <damian9203@gmail.com>
2021-12-19 14:28:38 +0100

    Add project files.

---
+++ L2_8_Zad_3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace L2_8_Zad_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int endNumber = 10000;
+            int fibSeqOne = 0;
+            int fibSeqTwo = 1;
+
+            Console.WriteLine("Ciąg Fibonacciego:");
+
+            for (int i = 0; fibSeqTwo < endNumber; i++)
+            {
+                fibSeqTwo = fibSeqOne + fibSeqTwo;
+                fibSeqOne = fibSeqTwo - fibSeqOne;
+                Console.WriteLine(fibSeqOne);
+            }
+        }
+    }
+}
